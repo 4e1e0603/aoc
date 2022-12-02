1e92868b4e8afec10bbf3876f06888f6b4afeefa
David Landa <david.landa@protonmail.com>
2022-12-02 18:52:20 +0100

    Cleanup

--- aoc2022d1.py
+++ aoc2022d1.py
@@ -3,17 +3,12 @@ Advent of Code 2022 Day 1
 """
 
 # v1
-
 xs = (x.split("\n") for x in open("./aoc2022d1.txt").read().split("\n\n"))
 
 ys = [sum(int(n) for n in x) for x in xs]
 
 s1 = max(ys)
-
 print(s1)
 
-# ---
-
 s2 = sum(sorted(ys, reverse=True)[:3])
-
 print(s2)
--- aoc2022d2.py
+++ aoc2022d2.py
@@ -3,25 +3,18 @@ Advent of Code 2022 Day 2
 """
 
 # v1
-
 xs = open("./aoc2022d2.txt").read().split("\n")
-
 R, P, S, L, D, W = 1, 2, 3, 0, 3, 6
 
 G1 = {"A X": R + D, "A Y": P + W, "A Z": S + L,
       "B X": R + L, "B Y": P + D, "B Z": S + W,
       "C X": R + W, "C Y": P + L, "C Z": S + D, }
-
 s1 = sum((G1[x] for x in xs))
-
 print(s1)
-
-# ---
 
 G2 = {"A X": S + L, "A Y": R + D, "A Z": P + W,
       "B X": R + L, "B Y": P + D, "B Z": S + W,
       "C X": P + L, "C Y": S + D, "C Z": R + W, }
 
 s2 = sum((G2[x] for x in xs))
-
 print(s2)
