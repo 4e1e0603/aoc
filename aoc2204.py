3848bae27989ab094b9b038f5caa1c0ef6328072
David Landa <david.landa@protonmail.com>
2022-12-04 14:35:20 +0100

    Simplify

--- aoc2204.py
+++ aoc2204.py
@@ -7,14 +7,11 @@ data = open("aoc2204.txt").read().strip().split()
 xs: list[tuple[set, set]] = []
 
 for d in data:
-    t = d.split(",")
-    x, y = tuple(map(int, t[0].split("-"))), tuple(map(int, t[1].split("-")))
+    x, y = [tuple(map(int, t.split("-"))) for t in d.split(",")]
     a, b = set(range(x[0], x[1] + 1)), set(range(y[0], y[1] + 1))
-    if b > a:
-        a, b = b, a
     xs.append((a, b))
 
-s1 = sum((a.issuperset(b) for a, b in xs))
+s1 = sum((a.issubset(b) or a.issuperset(b) for a, b in xs))
 print(s1)
 
 s2 = sum((bool(a.intersection(b)) for a, b in xs))
